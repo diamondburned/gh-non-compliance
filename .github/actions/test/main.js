0d67fa09e6d81f8a2255f7513faf672afaf52d04
diamondburned <diamond@arikawa-hi.me>
2022-11-25 15:06:52 -0800

    Eve more

--- .github/actions/test/main.js
+++ .github/actions/test/main.js
@@ -1,7 +1,7 @@
 const child_process = require("node:child_process")
 
 // able to get the input here.
-console.log("nodejs: camel-cased is", process.env["INPUT_CAMEL_CASED"])
+console.log("nodejs: camel_cased is", process.env["INPUT_CAMEL_CASED"])
 console.log("nodejs: kebab-cased is", process.env["INPUT_KEBAB-CASED"])
 // exec the shell script with the same env
 child_process.execSync(".github/actions/test/main.sh", { env: process.env, stdio: "inherit" })
--- .github/actions/test/main.sh
+++ .github/actions/test/main.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # This works.
-echo "camel_cased = ${INPUT_CAMEL_CASED}"
+echo "bash: camel_cased = ${INPUT_CAMEL_CASED}"
 # This does not work.
-echo "kebab-cased = ${INPUT_KEBAB-CASED}"
+echo "bash: kebab-cased = ${INPUT_KEBAB-CASED}"
