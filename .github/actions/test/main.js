219e9b8a763d24e46ce1396330a1fb5227ccfa8a
diamondburned <diamond@arikawa-hi.me>
2022-11-25 15:03:13 -0800

    such consistent path behavior, much wow

--- .github/actions/test/main.js
+++ .github/actions/test/main.js
@@ -4,4 +4,4 @@ const child_process = require("node:child_process")
 console.log("nodejs: kebab-cased is", process.env["INPUT_KEBAB-CASED"])
 console.log("nodejs: camel-cased is", process.env["INPUT_CAMEL_CASED"])
 // exec the shell script with the same env
-child_process.execSync("main.sh", { env: process.env, stdio: "inherit" })
+child_process.execSync(".github/actions/test/main.sh", { env: process.env, stdio: "inherit" })
